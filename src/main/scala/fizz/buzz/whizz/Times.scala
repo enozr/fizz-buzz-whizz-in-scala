d294c759a62cf17ca1e6489c428ed7865bc4f7f8
horance <horance@outlook.com>
2016-09-20 16:43:14 +0800

    first test case

---
+++ src/main/scala/fizz/buzz/whizz/Times.scala
@@ -0,0 +1,5 @@
+package fizz.buzz.whizz
+
+class Times(n: Int, word: String) {
+  def apply(m: Int): String = word
+}
--- src/test/scala/fizz/buzz/whizz/RuleSpec.scala
+++ src/test/scala/fizz/buzz/whizz/RuleSpec.scala
@@ -3,9 +3,9 @@ package fizz.buzz.whizz
 import org.scalatest.{FunSpec, Matchers}
 
 class RuleSpec extends FunSpec with Matchers {
-  describe("World") {
-    it ("should not be work" ) {
-      true should be(false)
+  describe("fizz buzz whizz") {
+    it ("times(3) => Fizz") {
+      new Times(3, "Fizz").apply(3*2) should be("Fizz")
     }
   }
 }
