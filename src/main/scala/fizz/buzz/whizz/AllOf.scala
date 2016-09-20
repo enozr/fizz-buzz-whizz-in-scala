1b55f9c35f082acf04ea6baf3c480f7b6f7273a6
horance <horance@outlook.com>
2016-09-20 17:23:34 +0800

    refactor AllOf: using placehold for foldLeft

--- src/main/scala/fizz/buzz/whizz/AllOf.scala
+++ src/main/scala/fizz/buzz/whizz/AllOf.scala
@@ -2,5 +2,5 @@ package fizz.buzz.whizz
 
 case class AllOf(times: Times*) {
   def apply(n: Int): String =
-    times.foldLeft("") { (acc, t) => acc + t.apply(n) }
+    times.foldLeft("") { _ + _.apply(n) }
 }
