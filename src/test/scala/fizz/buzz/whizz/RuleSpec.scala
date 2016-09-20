8f5057452319ed3b5debc2b213fab079dacee70f
horance <horance@outlook.com>
2016-09-20 16:46:12 +0800

    refactor Times using case class

--- src/test/scala/fizz/buzz/whizz/RuleSpec.scala
+++ src/test/scala/fizz/buzz/whizz/RuleSpec.scala
@@ -5,7 +5,7 @@ import org.scalatest.{FunSpec, Matchers}
 class RuleSpec extends FunSpec with Matchers {
   describe("fizz buzz whizz") {
     it ("times(3) => Fizz") {
-      new Times(3, "Fizz").apply(3*2) should be("Fizz")
+      Times(3, "Fizz")(3*2) should be("Fizz")
     }
   }
 }
