da418cdbe06ceb1921d611efdde361083cd73cea
horance <horance@outlook.com>
2016-09-20 17:13:10 +0800

    refactor AllOf: type reference for foreach

--- src/main/scala/fizz/buzz/whizz/AllOf.scala
+++ src/main/scala/fizz/buzz/whizz/AllOf.scala
@@ -3,7 +3,7 @@ package fizz.buzz.whizz
 case class AllOf(times: Times*) {
   def apply(n: Int): String = {
     val result = new StringBuilder
-    times.foreach { t => result.append(t.apply(n)) }
+    times.foreach { t => result.append(t(n)) }
     result.toString
   }
 }
